f89ee15f2e37c163965b83ae5b8dee7b7c9c5bf1
Pro4TLZZ <github@pro4tlzz.com>
2022-01-07 18:23:42 +0000

    add function for getting record IDs, not working yet

--- get_records.py
+++ get_records.py
@@ -58,14 +58,25 @@ def get_records(api_key,zone_id):
         )
 
         jsonContent = json.loads(response.text)
-        print(json.dumps(jsonContent, indent=4))
+        #print(json.dumps(jsonContent, indent=4))
+        return(jsonContent)
     except:
         print("\033[1m"+"Issue Occured with getting records"+"\033[0m")
         sys.exit(1)
 
+def get_record_id(api_key,zone_id,records):
+    try:
+        for each in records:
+            record_id=records["id"]
+            print(record_id)
+            return record_id
+    except:
+        print("\033[1m"+"Issue Occured with getting record IDs"+"\033[0m")
+        sys.exit(1)
 
 
 dns_records='records.txt'
 #get_list_from_file(dns_records)
 #api_success_or_fail=check_api(api_key)
 remote_records=get_records(api_key,zone_id)
+remote_records_ids=get_record_id(api_key,zone_id,remote_records)
