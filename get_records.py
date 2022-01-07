b6fa3edde0b61fd6c0505a1cf51ef3bf9c41c3bf
Pro4TLZZ <github@pro4tlzz.com>
2022-01-07 18:12:21 +0000

    add query for A records only

--- get_records.py
+++ get_records.py
@@ -7,6 +7,18 @@ from requests import api
 api_key=sys.argv[1]
 zone_id=sys.argv[2]
 
+def get_list_from_file(filename):
+    try:
+        # open and read the file into list
+        with open(filename) as f:
+            string_list = f.read().splitlines()
+            f.close()
+            print(string_list)
+            return string_list
+    except:
+        print("\033[1m"+"Issue Occured with obtaining list from file"+"\033[0m")
+        sys.exit(1)
+
 def check_api(api_key):
     try:
         url = "https://api.cloudflare.com/client/v4/user/tokens/verify"
@@ -31,7 +43,7 @@ def check_api(api_key):
 
 def get_records(api_key,zone_id):
     try:
-        url = "https://api.cloudflare.com/client/v4/zones/"+ zone_id +"/dns_records"
+        url = "https://api.cloudflare.com/client/v4/zones/"+ zone_id +"/dns_records?type=A"
 
         headers = {
         "Accept" : "application/json",
@@ -46,10 +58,14 @@ def get_records(api_key,zone_id):
         )
 
         jsonContent = json.loads(response.text)
-        print(jsonContent)
+        print(json.dumps(jsonContent, indent=4))
     except:
         print("\033[1m"+"Issue Occured with getting records"+"\033[0m")
         sys.exit(1)
-		
-api_success_or_fail=check_api(api_key)
+
+
+
+dns_records='records.txt'
+#get_list_from_file(dns_records)
+#api_success_or_fail=check_api(api_key)
 remote_records=get_records(api_key,zone_id)
